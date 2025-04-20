928f7e283171e5ddcc0e02937f6c658bdf985024
TalhaTahir-09 <hamzatalhabwp4@gmail.com>
2025-04-21 01:46:50 +0500

    Learning modules

---
+++ Node/logger.js
@@ -0,0 +1,5 @@
+let url = "https://www.someurl.com"
+function log(message){
+    console.log(message)
+}
+module.exports = [1,2,3,4,5]
---
+++ Node/pathModule.js
@@ -0,0 +1,7 @@
+let path = require("node:path");
+// console.log(path.basename(`C:\\Users\\M.Talha Tahir\\Desktop\\Backend.js\\index.html`, ".html"));
+// console.log(path.dirname)
+// console.log(path.sep)
+// console.log(path.join("foo", "talha", "poo","soo"));
+console.log(path.normalize("//foo//tlaha/bear//me"));
+console.log(path.parse("C:\\Users\\M.Talha Tahir\\Desktop\\Backend.js"));
