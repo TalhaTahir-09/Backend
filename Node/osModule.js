896d262c4e61c33012c3e6520a48543eac086886
TalhaTahir-09 <hamzatalhabwp4@gmail.com>
2025-04-21 14:39:01 +0500

    Added comments

--- Node/osModule.js
+++ Node/osModule.js
@@ -1,16 +1,15 @@
-const os = require('os')
+const os = require('os');
 
-let totalMemory = (os.totalmem() / 1024**3);
-let freeMemory = os.freemem() /  1024**3
-// console.log(`Total memory is: ${totalMemory.toFixed(2)}`)
-// console.log(`Free memory is: ${freeMemory.toFixed(2)}`)
-let cpuInfo = os.cpus();
-// console.log(cpuInfo)
-// console.log(os.hostname());
-// console.log(os.machine())
-// console.log(os.networkInterfaces())
-// console.log(os.platform())
-// console.log(os.tmpdir())
-// console.log(os.platform())
-// console.log((os.uptime() / 60).toFixed(0))
-console.log(os.userInfo())
+let totalMemory = os.totalmem() / 1024 ** 3; // total RAM in GB
+let freeMemory = os.freemem() / 1024 ** 3;   // free RAM in GB
+
+let cpuInfo = os.cpus();// CPU details
+console.log(cpuInfo);
+
+console.log(os.hostname()); // system name
+console.log(os.machine()); // CPU architecture (cores and name)
+console.log(os.networkInterfaces());// network info (ethernet, wift, Ips)
+console.log(os.platform());// OS platform (win32, posix etx)
+console.log(os.tmpdir());// temporary files path 
+console.log((os.uptime() / 60).toFixed(0)); // uptime in mins
+console.log(os.userInfo()); // current user info
